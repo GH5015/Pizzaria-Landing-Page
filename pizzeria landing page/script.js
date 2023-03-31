265c66d4097910889bc9f0dd4843f1b211d35ee7
Gustavo H. Lemos <gustavhenrique79@gmail.com>
2023-03-31 18:15:26 -0300

    Add files via upload

---
+++ pizzeria landing page/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('butao-menu');
+const div = document.getElementById('menu');
+
+button.addEventListener('click', () => {
+    div.style.display = 'flex';
+})
+  
